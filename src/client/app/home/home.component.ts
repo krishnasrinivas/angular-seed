afeaaf2c3c1b9dd181dd9a2a652b16a4b002a3aa
Krishna Srinivas <krishna.srinivas@gmail.com>
2017-06-14 17:24:51 -0700

    Minio with TS

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NameListService } from '../shared/name-list/name-list.service';
+import * as minio from './minio-browser.js'
 
 /**
  * This class represents the lazy loaded HomeComponent.
@@ -15,7 +16,6 @@ export class HomeComponent implements OnInit {
   newName: string = '';
   errorMessage: string;
   names: any[] = [];
-
   /**
    * Creates an instance of the HomeComponent with the injected
    * NameListService.
@@ -48,6 +48,26 @@ export class HomeComponent implements OnInit {
    */
   addName(): boolean {
     // TODO: implement nameListService.post
+    var client = new minio.Client({
+      endPoint: "localhost",
+      port: 9000,
+      secure: false,
+      accessKey: "SXO8VW2OFKKP2OG7AC85",
+      secretKey: "CKWSSgrUgvfUMTaNBkB63exet4WW+uNhQvi91Bc3",
+      region: "us-east-1",
+    });
+    client.getObject('test', 'passwd', function(e, dataStream) {
+      if (e) {
+          return console.log(e)
+      }
+      dataStream.on('data', function(data) {
+          console.log(data.toString())
+      })
+      dataStream.on('error', function(e) {
+          console.log('oops')
+      })
+    })
+
     this.names.push(this.newName);
     this.newName = '';
     return false;
